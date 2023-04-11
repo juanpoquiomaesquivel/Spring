959b0435f88e2e4e81795751a494aa155a8645a1
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-04-11 15:14:51 -0500

    SpringMVC[Beginners] - 5 - ViewResolver, view using SpringFramework

--- selenium express[Spring MVC-Beginners]/myfirst-mvc.project/src/main/java/com/home/controllers/BroController.java
+++ selenium express[Spring MVC-Beginners]/myfirst-mvc.project/src/main/java/com/home/controllers/BroController.java
@@ -2,14 +2,14 @@ package com.home.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BroController {
 
-	@ResponseBody
 	@RequestMapping("/cricketbat")
 	public String giveCricketBat() {
-		return "hey man.. this is your cricket bat.";
+		return "MRFCricketbat";
 	}
 }
+
+// https://www.youtube.com/watch?v=5_G1jIiK2hI&list=PL3NrzZBjk6m-OSeL3DRMD_ISa0u-9c1l1&index=8
